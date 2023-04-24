2c1e743df005f49a54969b49d7220a67e231970e
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-04-24 12:45:33 +0100

    Show added ticket

--- src/bin/db_test.rs
+++ src/bin/db_test.rs
@@ -10,13 +10,25 @@ async fn main() -> Result<()> {
 
     let id = create_ticket(&db).await?;
 
-    println!("\nAdded new Ticket ID: {}\n", id);
+    println!("\nAdded new Ticket ID: {}", id);
+
+    let added_ticket = get_ticket(&db, id).await?;
+
+    if let Some(ticket) = added_ticket {
+        println!(
+            "\tTicket ID: {}, Created_by_uid: {}, Title: {}, Done: {}\n",
+            ticket.id, ticket.created_by_uid, ticket.title, ticket.done
+        );
+    } else {
+        println!("ERROR: Failed to get added ticket");
+    }
 
     let tickets = get_tickets(&db).await?;
 
+    println!("All Tickets");
     for ticket in tickets {
         println!(
-            "Ticket ID: {}, Created_by_uid: {}, Title: {}, Done: {}",
+            "\tID: {}, Created_by_uid: {}, Title: {}, Done: {}",
             ticket.id, ticket.created_by_uid, ticket.title, ticket.done
         );
     }
@@ -56,6 +68,25 @@ async fn create_ticket(db: &Database) -> Result<i64> {
     .last_insert_rowid();
 
     Ok(ticket_id)
+}
+
+async fn get_ticket(db: &Database, id: i64) -> Result<Option<Ticket>> {
+    let ticket = sqlx::query!(
+        r#"SELECT * FROM tickets
+        WHERE id = ?1"#,
+        id
+    )
+    .fetch_optional(&db.pool)
+    .await?;
+
+    Ok(ticket.map_or(None, |rec| {
+        Some(Ticket {
+            id: rec.id as u64,
+            created_by_uid: rec.created_by_uid as u64,
+            title: rec.title.clone(),
+            done: rec.done,
+        })
+    }))
 }
 
 async fn get_tickets(db: &Database) -> Result<Vec<Ticket>> {
