54dffece1793fdbdb05e8ad80a848a7976c7ae54
Shahzad Naeem <shahzad65naeem@gmail.com>
2023-04-24 12:22:25 +0100

    Create added

--- src/bin/db_test.rs
+++ src/bin/db_test.rs
@@ -1,10 +1,18 @@
+use chrono::prelude::*;
+use core::time;
+use std::thread;
+
 use axum_tut::{database::Database, error::Result, model::tickets::Ticket};
 
 #[tokio::main]
 async fn main() -> Result<()> {
-    let pool = Database::new().await;
+    let db = Database::new().await;
 
-    let tickets = get_tickets(&pool).await?;
+    let id = create_ticket(&db).await?;
+
+    println!("\nAdded new Ticket ID: {}\n", id);
+
+    let tickets = get_tickets(&db).await?;
 
     for ticket in tickets {
         println!(
@@ -14,6 +22,40 @@ async fn main() -> Result<()> {
     }
 
     Ok(())
+}
+
+fn new_ticket_title() -> String {
+    thread::sleep(time::Duration::from_secs(1));
+    let local_now = Local::now();
+    let title_text = local_now.format("Ticket created at %H:%M:%S").to_string();
+
+    title_text
+}
+
+fn random_uid() -> i64 {
+    static mut NEXT_UID: i64 = 100;
+
+    unsafe {
+        NEXT_UID += 1;
+        NEXT_UID
+    }
+}
+
+async fn create_ticket(db: &Database) -> Result<i64> {
+    let uid = random_uid();
+    let title = new_ticket_title();
+
+    let ticket_id = sqlx::query!(
+        r#"INSERT INTO tickets(created_by_uid, title)
+        VALUES(?1,?2)"#,
+        uid,
+        title
+    )
+    .execute(&db.pool)
+    .await?
+    .last_insert_rowid();
+
+    Ok(ticket_id)
 }
 
 async fn get_tickets(db: &Database) -> Result<Vec<Ticket>> {
